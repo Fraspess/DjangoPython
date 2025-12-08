ab21a64c4a88a28ffda676cdc1abd39a05c01197
Fraspess <pavlosamchyk@gmail.com>
2025-12-09 00:08:07 +0200

    bootstrap validation for all fields including tinymce and redesigned category page

--- djangomvt/categories/views.py
+++ djangomvt/categories/views.py
@@ -1,3 +1,4 @@
+import datetime
 from django.shortcuts import render
 from django.shortcuts import redirect
 from .models import Category
@@ -14,12 +15,14 @@ def add_category(request):
     print(request)
     if request.method == "POST":
         category_name = request.POST.get("name")
+        print(category_name)
         category_slug = slugify(unidecode(category_name))
         category_description = request.POST.get("description")
-        category_created_at = request.POST.get("created_at")
-        category_updated_at = request.POST.get("updated_at")
         category_is_active = request.POST.get("is_active") == "checked"
         category_image = request.FILES.get("image")
+
+        category_created_at = datetime.datetime.now()
+        category_updated_at = datetime.datetime.now()
 
         category = Category(name = category_name, slug = category_slug, description = category_description, created_at = category_created_at, updated_at = category_updated_at, is_active = category_is_active, image = category_image)
         category.save()
