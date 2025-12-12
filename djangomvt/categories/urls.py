2ba1ee0214be7b6c34a9085a9403b6201a6fc742
Fraspess <pavlosamchyk@gmail.com>
2025-12-13 00:54:08 +0200

    done

--- djangomvt/categories/urls.py
+++ djangomvt/categories/urls.py
@@ -5,5 +5,7 @@ app_name="categories"
 
 urlpatterns = [
     path('', views.show_categories, name='show_categories'),
-    path('add', views.add_category, name='add_category'),
+    path('add/', views.add_category, name='add_category'),
+    path('delete/',views.delete_category, name='delete_category'),
+    path('edit/',views.edit_category, name='edit_category'),
 ]
